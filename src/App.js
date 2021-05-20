cda202339bf0ee7202749c1fdfeb6ed075963762
Elidon <30506155+elidondemolli@users.noreply.github.com>
2021-05-20 19:56:47 +0200

    added light/dark mode

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import CountryPicker from './components/CountryPicker/CountryPicker'
 import { fetchData } from './api/index'
 import styles from './App.module.css'
 import coronaImage from './images/COVID-19 icon.png'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { Paper, Switch, CssBaseline } from '@material-ui/core'
 class App extends React.Component {
   state = {
     data: {},
     country: '',
+    darkMode: false
   }
 
   async componentDidMount() {
@@ -22,15 +25,34 @@ class App extends React.Component {
     this.setState( {data: fetchedData, country: country} )
   }
 
+  changeTheme = () => {
+    if(this.state.darkMode){
+      this.setState( {darkMode: false} )
+    } else {
+      this.setState( {darkMode: true} )
+    }
+  }
+
   render() {
+    const theme = createMuiTheme({
+      palette: {
+        type: this.state.darkMode ? "dark" : "light"
+      }
+    })
+    
     const { data, country } = this.state
     return (
-      <div className = {styles.container} >
-        <img className={styles.image} src={coronaImage}></img>
-        <Cards data = {data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart data={data} country={country}/>
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <div className = {styles.container} >
+            <Switch className={styles.switch1} checked={this.state.darkMode} onChange={this.changeTheme}></Switch>
+            <img className={styles.image} src={coronaImage}></img>
+            <Cards data = {data} />
+            <CountryPicker handleCountryChange={this.handleCountryChange}/>
+            <Chart data={data} country={country}/>
+          </div>
+        </CssBaseline>
+      </ThemeProvider>
     )
   }
 }
